fc0c0726c090f61b86372d1e052e954864f226eb
zuck43 <110517938+zuck43@users.noreply.github.com>
2024-09-30 12:08:31 +0500

    Update stream.py

--- biisal/bot/plugins/stream.py
+++ biisal/bot/plugins/stream.py
@@ -80,7 +80,7 @@ async def private_receive_handler(c: Client, m: Message):
         return await m.reply(Var.BAN_ALERT)
     try:
         log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
-        stream_link = f"{Var.URL}watch/{str(log_msg.id)}/?hash={get_hash(log_msg)}{quote_plus(get_name(log_msg))}"
+        stream_link = f"{Var.URL}watch/{str(log_msg.id)}/{get_hash(log_msg)}/{quote_plus(get_name(log_msg))}"
         online_link = f"{Var.URL}{str(log_msg.id)}/?hash={get_hash(log_msg)}{quote_plus(get_name(log_msg))}"
         await log_msg.reply_text(text=f"**RᴇQᴜᴇꜱᴛᴇᴅ ʙʏ :** [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n**Uꜱᴇʀ ɪᴅ :** `{m.from_user.id}`\n**Stream ʟɪɴᴋ :** {stream_link}", disable_web_page_preview=True,  quote=True)
         await m.reply_text(
