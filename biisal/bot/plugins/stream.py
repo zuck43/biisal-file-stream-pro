28be03ec5af57310c3587da40971569a26ef5b40
zuck43 <110517938+zuck43@users.noreply.github.com>
2024-09-30 12:17:20 +0500

    Update stream.py

--- biisal/bot/plugins/stream.py
+++ biisal/bot/plugins/stream.py
@@ -81,7 +81,7 @@ async def private_receive_handler(c: Client, m: Message):
     try:
         log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
         stream_link = f"{Var.URL}watch/{str(log_msg.id)}/{get_hash(log_msg)}/{quote_plus(get_name(log_msg))}"
-        online_link = f"{Var.URL}{str(log_msg.id)}/?hash={get_hash(log_msg)}{quote_plus(get_name(log_msg))}"
+        online_link = f"{Var.URL}{str(log_msg.id)}/{get_hash(log_msg)}/{quote_plus(get_name(log_msg))}"
         await log_msg.reply_text(text=f"**RᴇQᴜᴇꜱᴛᴇᴅ ʙʏ :** [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n**Uꜱᴇʀ ɪᴅ :** `{m.from_user.id}`\n**Stream ʟɪɴᴋ :** {stream_link}", disable_web_page_preview=True,  quote=True)
         await m.reply_text(
             text=msg_text.format(get_name(log_msg), humanbytes(get_media_file_size(m)), online_link, stream_link),
